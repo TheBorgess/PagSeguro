f13029f19a552814c2e19eab21cfd2371bf38186
TheBorgess <62522580+TheBorgess@users.noreply.github.com>
2021-11-07 00:38:32 -0300

    Update of Notifications in javascript and php.

--- news.js
+++ news.js
@@ -1,37 +1,52 @@
 
 const mostraNotificacao = () => {
-    const notification = new Notification('Backup News of Netflix!', {
-    	
+  var nome = document.getElementById('nome').value;
+  if (nome != 'Gremio'){  
+  
+    const notification = new Notification(nome, {
       body: 'Go to www.netflix.com',
-    	icon: '../DAO/images/netflix.jpg'
-
+    	icon: '../DAO/images/login.jpg',
+      image: '../DAO/images/netflix.jpg',
+      badge: '../DAO/images/logo_php.jpg'
     });
 
+    //setTimeout(() => notification.close(), 1000*1000);
+    //setTimeout(() => {
+    //    notification.close();
+    // }, 100 * 1000);
+
     notification.onclick = (e) => {
-      //e.preventDefault();
-      //window.focus();
       window.open("https://www.netflix.com");
-      //////window.location.href = 'www.netflix.com';
       notification.close();
+    }
+  
+  } else{
+  
+      const notification = new Notification(nome, {
+        body: 'Go to www.gremio.net',
+        icon: '../DAO/images/gremio.jpg',
+        image: '../DAO/images/arenaGremio.jpg'
+      });
+   
+      notification.onclick = (e) => {
+        window.open("https://www.gremio.net");
+        notification.close();
+      }
+  
     }
 
 };
 
 const validaNotificacao = () => {
-	
   if (Notification.permission === 'granted'){
        mostraNotificacao();
   } else if (Notification.permission !== 'denied'){
      Notification.requestPermission(function (permission) {
-       
        if (permission === 'granted'){
             mostraNotificacao();
        }
-       
      });
-
     } 
-
 };
 
 document.getElementById('notificar').addEventListener('click', validaNotificacao);
